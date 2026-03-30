9acd2a24d4f2bb6fe1a9edf6a68804cf83eda6e7
5s9tnn49cm-hub <5s9tnn49cm@privaterelay.appleid.com>
2026-03-30 09:33:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'set-tracker-v5';
+const CACHE_NAME = 'set-tracker-v6';
 const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
